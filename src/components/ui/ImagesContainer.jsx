afa2c3d8ced4be6433c8c1bdbea1736fd2bd843a
Bhavani Bolloju <bhavani.bolloju@gmail.com>
2024-05-26 13:13:05 +0530

    add bookspotlight mobile view image

--- src/components/ui/ImagesContainer.jsx
+++ src/components/ui/ImagesContainer.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 // import React from "react";
 import classes from "./ImagesContainer.module.scss";
 
@@ -5,10 +6,10 @@ function ImagesContainer(props) {
   return (
     <div className={classes.images}>
       <div className={classes.image1}>
-        <img src={props.image1} alt="" />
+        <img src={props.image1} alt="desktop view" width={500} height={400} />
       </div>
       <div className={classes.image2}>
-        <img src={props.image2} alt="" />
+        <img src={props.image2} alt="mobile view" width={200} height={500} />
       </div>
     </div>
   );
