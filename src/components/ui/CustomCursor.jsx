00667a9ca5bd60180a946ed4cd0ac8cbdc31f9bb
Bhavani <100474477+Bhavani-Bolloju@users.noreply.github.com>
2025-03-15 23:13:32 +0530

    Update CustomCursor.jsx
    Add throttling to cursor movement

--- src/components/ui/CustomCursor.jsx
+++ src/components/ui/CustomCursor.jsx
@@ -1,20 +1,36 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import classes from "./CustomCursor.module.scss";
 
 function CustomCursor() {
   const [cursorX, setCursorX] = useState(0);
   const [cursorY, setCursorY] = useState(0);
 
+  const throttlingState = useRef(true);
+  const timeOutId = useRef(null);
+  const cursorPoints = useRef({ x: 0, y: 0 });
+
   useEffect(() => {
     const updateCursorPosition = function (e) {
-      setCursorX(e.clientX);
-      setCursorY(e.clientY);
+      cursorPoints.current = { x: e.clientX, y: e.clientY };
+
+      // console.log(cursorPoints.current);
+
+      if (throttlingState.current === false) return;
+
+      throttlingState.current = false;
+
+      timeOutId.current = setTimeout(() => {
+        throttlingState.current = true;
+        setCursorX(cursorPoints.current.x);
+        setCursorY(cursorPoints.current.y);
+      }, 150);
     };
 
     window.addEventListener("mousemove", updateCursorPosition);
 
     return () => {
       window.removeEventListener("mousemove", updateCursorPosition);
+      if (timeOutId.current) clearTimeout(timeOutId.current);
     };
   }, []);
 
